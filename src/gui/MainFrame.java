388692877078e98a71a8aa50fac19c096fd93491
Matt Ellis <mattellis11@gmail.com>
2023-02-21 07:15:58 -0600

    updated comments

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,28 +1,27 @@
-/**
- * MainFrame.java
- * @author Matt Ellis
- * Creates JFrame of Patient Collection gui
- */
-
 package gui;
 
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+/**
+ * Creates frontend GUI for a patient collection
+ *
+ * @author Matt Ellis
+ */
 public class MainFrame {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Patient Collection Manager");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(815, 538));
-		frame.setResizable(false);
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Patient Collection Manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(815, 538));
+        frame.setResizable(false);
 
-		frame.getContentPane().add(new MainPanel());
+        frame.getContentPane().add(new MainPanel());
 
-		frame.pack();
-		frame.setVisible(true);
+        frame.pack();
+        frame.setVisible(true);
 
-	}
+    }
 
 }
