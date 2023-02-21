60418053003619b2f0d45e521b416ddda0d64211
Matt Ellis <mattellis11@gmail.com>
2023-02-21 07:19:53 -0600

    updated comments

---
+++ tests/backend/PatientCollectionTest.java
@@ -0,0 +1,28 @@
+package backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PatientCollectionTest {
+
+    @Test
+    public void removePatient() {
+    }
+
+    @Test
+    public void setResultForPatient() {
+    }
+
+    @Test
+    public void getIds() {
+    }
+
+    @Test
+    public void addPatientsFromFile() {
+    }
+
+    @Test
+    public void writeFile() {
+    }
+}
